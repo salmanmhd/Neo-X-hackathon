6e239599a090a7b29fea194a9737ce5b5f9754ae
Arun <vikrantkumarprajapati4@gmail.com>
2024-09-29 12:37:31 +0530

    Some Ui and icon chang

--- ActiveNeo/src/components/Dashboard.jsx
+++ ActiveNeo/src/components/Dashboard.jsx
@@ -41,7 +41,7 @@ export default function Dashboard() {
     <div className="bg-gradient-to-br from-purple-900 to-black">
       {" "}
       <Navbar />
-      <div className="flex justify-center items-center min-h-screen mt-5 text-white">
+      <div className="flex justify-center items-center min-h-screen mt-5 text-white pb-10">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <Card className="bg-gradient-to-br from-purple-800 to-gray-900 border-purple-600">
             <CardHeader>
