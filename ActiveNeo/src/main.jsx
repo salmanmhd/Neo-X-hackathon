0825b305f54354da72f18c735df6798634b32d83
Md Salman <mhdsalman010@gmail.com>
2024-09-29 10:47:19 +0530

    basic changes

--- ActiveNeo/src/main.jsx
+++ ActiveNeo/src/main.jsx
@@ -1,16 +1,12 @@
-
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 import './index.css';
 import Dashboard from './components/Dashboard.jsx';
 import Tracker from './components/tracker/Tracker.jsx';
 import CaptureImage from './components/tracker/ImageCapture.jsx';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     {/* <App /> */}
