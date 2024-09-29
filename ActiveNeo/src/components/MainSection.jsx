3f9d3ae4e46994f573c84fdd8a55dbefe61bd686
Arun <vikrantkumarprajapati4@gmail.com>
2024-09-29 11:07:54 +0530

    main section changes

--- ActiveNeo/src/components/MainSection.jsx
+++ ActiveNeo/src/components/MainSection.jsx
@@ -14,7 +14,7 @@ function MainSection() {
         </p>
 
         <div className="flex space-x-4 mt-8">
-          <button className="px-4 py-2 border border-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2 hover:bg-gradient-to-r from-purple-500 to-black">
+          <button className="px-6 py-3 border border-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2 hover:bg-gradient-to-r from-purple-500 to-black">
             <span>Start Tracking</span>
           </button>
         </div>
